6a847f8d80c8cb22ac72e323cdcc2f8bc7d2b7eb
fmaj7 <fmaj7@live.com>
2012-02-07 08:08:51 -0800

    update jQuery to 1.7.1;change up home tempate;changed login tempate;Added myHome template;Small UI refinements

--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 
-@login_required
 def home(request):
-    return render_to_response('home/home.html', RequestContext(request, {}))
+    form = AuthenticationForm(request)
+    request.session.set_test_cookie()
+    c = {'form': form}
+    return render_to_response('home/home.html', RequestContext(request, c))
