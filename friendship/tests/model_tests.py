865a9b3e1290b80afd3a0297cbbd9a54f5b94207
fmaj7 <fmaj7@live.com>
2012-03-19 23:47:56 -0700

    Cleaning up pinax branch to have a fresh start

--- friendship/tests/model_tests.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase
-
-class FriendshipModelTestCases(TestCase):
-    ''' Testing for Friendship Model
-    
--- registration/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-from view_tests import *
-from model_tests import *
-from form_tests import *
