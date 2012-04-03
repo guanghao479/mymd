59abfd521a126295541bc99daaec24ae3b5017cc
Xinghan Cui <xinghan.cui@gmail.com>
2012-04-02 21:43:44 -0400

    Fix issue #30 Override idios default profile module to our own profile module.

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+
 from idios.models import ProfileBase
 
 
 class Profile(ProfileBase):
-    name = models.CharField(_("name"), max_length=50, null=True, blank=True)
-    about = models.TextField(_("about"), null=True, blank=True)
-    location = models.CharField(_("location"), max_length=40, null=True, blank=True)
-    website = models.URLField(_("website"), null=True, blank=True, verify_exists=False)
+    """
+    User profile class. Inherit from ProfileBase of idios.
+    """
+    GENDER_CHOICES = (
+        (u'M', u'Male'),
+        (u'F', u'Female'),
+        )
+    city = models.CharField(_("city"), max_length=50)
+    gender = models.CharField(max_length=2, choices=GENDER_CHOICES)
+    birth_date = models.DateField(null=True)
