c2e0f2397e72c5537bf669472efdd9a5b3fca19d
fmaj7 <fmaj7@live.com>
2012-06-30 21:36:26 -0700

    comment out search, which requires xapian library dependencies

--- apps/profiles/search_indexes.py
+++ apps/profiles/search_indexes.py
@@ -1,8 +1,8 @@
-from haystack.indexes import *
-from haystack import site
+#from haystack.indexes import *
+#from haystack import site
 #from profile.models import Profile
 
-class ProfileIndex(SearchIndex):
-    text = CharField(document=True, use_template=True)
-    city = CharField(model_attr='city')
-    disease = CharField(model_attr='disease')
+#class ProfileIndex(SearchIndex):
+#    text = CharField(document=True, use_template=True)
+#    city = CharField(model_attr='city')
+#    disease = CharField(model_attr='disease')
