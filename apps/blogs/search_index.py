e3d13a7231712c2a454c61bc6e50e1c04c52f6bb
Xinghan Cui <xinghan.cui@gmail.com>
2012-07-06 01:06:40 -0400

    Fix search index item.

--- apps/blogs/search_index.py
+++ apps/blogs/search_index.py
@@ -5,8 +5,8 @@ from blogs.models import Post
 
 class PostIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    author = indexes.CharField(model_attr='user')
-    pub_date = indexes.DateTimeField(model_attr='pub_date')
+    body = indexes.TextField(model_attr='body')
+    title = indexes.CharField(model_attr='title')
 
     def get_model(self):
         return Post
