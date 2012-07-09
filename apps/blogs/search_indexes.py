594e9d03b60ce7f15d4c802554c7737d5f5abd31
fmaj7 <fmaj7@live.com>
2012-07-08 19:32:10 -0700

    Search is working, the url of the search result is not the url to the blog

--- apps/blogs/search_indexes.py
+++ apps/blogs/search_indexes.py
@@ -2,9 +2,8 @@ import datetime
 from haystack import indexes
 from blogs.models import Post
 
-
 class PostIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=False)
+    text = indexes.CharField(document=True, use_template=True)
     body = indexes.CharField(model_attr='body')
     title = indexes.CharField(model_attr='title')
 
--- apps/profiles/search_indexes.py
+++ apps/profiles/search_indexes.py
@@ -1,8 +1,9 @@
-#from haystack.indexes import *
-#from haystack import site
-#from profile.models import Profile
-
-#class ProfileIndex(SearchIndex):
-#    text = CharField(document=True, use_template=True)
-#    city = CharField(model_attr='city')
-#    disease = CharField(model_attr='disease')
+#from haystack import indexes
+#from profiles.models import Profile
+#
+#class ProfileIndex(indexes.SearchIndex, indexes.Indexable):
+#    text = indexes.CharField(document=True, use_template=True)
+#    city = indexes.CharField(model_attr='city')
+#
+#    def get_model(self):
+#        return Profile
