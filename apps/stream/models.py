228d157aea43f075810c2904267fb6b6f4cc7864
fmaj7 <fmaj7@live.com>
2012-07-18 21:13:05 -0700

    adding following to friends request signal handling

--- apps/stream/models.py
+++ apps/stream/models.py
@@ -3,12 +3,14 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from friends.models import FriendshipInvitation
 from friends.signals import *
-from actstream import action
+from actstream import action, follow
 
 @receiver(friends_connected)
 def friend_connected_action(sender, **kwargs):
     action.send(sender.from_user, verb=u'connected', action_object=sender.to_user)
     action.send(sender.to_user, verb=u'connected', action_object=sender.from_user)
+    follow(sender.from_user, sender.to_user)
+    follow(sender.to_user, sender.from_user)
 
 @receiver(friends_requested)
 def friend_requested_action(sender, **kwargs):
