415adde5cd11838f837fcf1c78db45cc04d35b2a
Xinghan Cui <xinghan.cui@gmail.com>
2012-10-29 12:00:33 -0400

    Add new meetup test cases. Modify meetup template to display new line in details meetup content.

--- meetup/tests/models.py
+++ meetup/tests/models.py
@@ -2,7 +2,7 @@ from datetime import date
 
 from django.test import TestCase
 
-from meetup.models import Meetup
+from meetup.models import Meetup, Attend
 from city.models import City
 from django.contrib.auth.models import User
 
@@ -10,7 +10,7 @@ class MeetupTestCase(TestCase):
 
     def setUp(self):
         self.city = City.objects.create(country='China', name='Beijing', size=100)
-        self.organizer = User.objects.create_user(username='Xinghan', email='xinghan@gmail.com')
+        self.organizer = User.objects.create_user(username='Xinghan', email='xinghan@email.com')
         self.address = 'Yayuncun, starbucks'
         self.hold_date = date(2012, 10,12)
         self.title = 'loveandhelp group discussion'
@@ -26,14 +26,36 @@ class MeetupTestCase(TestCase):
                                        created_date = self.created_date,
                                        modified_date = self.modified_date)
 
-    def test_meetup(self):
+    def test_meetup_model(self):
         """
         Tests for meetup model.
 
         """
         self.assertEquals(Meetup.objects.all().count(), 1)
         self.assertEquals(self.meetup.title, 'loveandhelp group discussion')
+        self.assertEquals(self.meetup.content, 'loveandhelp group discussion')
         self.assertEquals(self.meetup.city.name, 'Beijing')
-        
+        self.assertEquals(self.meetup.organizer.username, 'Xinghan')
+        self.assertEquals(self.meetup.address, 'Yayuncun, starbucks')
+        self.assertEquals(self.meetup.date, date(2012, 10,12))
+        self.assertEquals(self.meetup.created_date, date(2012, 10, 9))
+        self.assertEquals(self.meetup.modified_date, date(2012, 10, 9))
+
+    def test_meetup_attend(self):
+        """
+        Test cases for meetup attend model.
+
+        """
+        self.attender1 = User.objects.create_user(username='Lucy', email='lucy@email.com')
+        self.attender2 = User.objects.create_user(username='Guanghao', email='guanghao@email.com')
+        self.attend_relationship = Attend.objects.create(attender = self.attender1,
+                                                    meetup = self.meetup,
+                                                    attend_date = date(2012, 10, 13))
+        self.assertEquals(self.attend_relationship.attender.username, 'Lucy')
+        self.assertEquals(self.attend_relationship.meetup.title, 'loveandhelp group discussion')
+        self.assertEquals(self.attend_relationship.attend_date, date(2012, 10, 13))
+        self.assertTrue(Attend.objects.is_attent(self.attender1, self.meetup))
+        self.assertFalse(Attend.objects.is_attent(self.attender2, self.meetup))
+
 
 
