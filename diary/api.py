f01bcd2bd67e6510b9a041577d703410551baef7
fmaj7 <fmaj7@live.com>
2012-12-13 21:22:56 -0800

    #109 Upgrade django-tastypie to the latest version so that we can use sessionAuth

--- diary/api.py
+++ diary/api.py
@@ -2,10 +2,13 @@ from tastypie.resources import ModelResource
 from tastypie import fields
 from diary.models import Diary
 from mymd.api import UserResource
+from tastypie.authentication import SessionAuthentication
 
 class DiaryResource(ModelResource):
-    author = fields.ForeignKey(UserResource, 'author')
+    author = fields.ForeignKey(UserResource, 'author', full=True)
 
     class Meta:
         queryset = Diary.objects.all()
         resource_name = 'diary'
+        authentication = SessionAuthentication()
+
