0c2ddc70a4521fb9f60ed6a6799bbdfb41e900b9
fmaj7 <fmaj7@live.com>
2012-12-25 23:11:26 -0800

    #114 Adding redactor to experience

--- experiences/forms.py
+++ experiences/forms.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.models import User
 from django import forms
 from experiences.models import Post
-
+from redactor.widgets import RedactorEditor
+from django.conf import settings
 
 class PostForm(forms.ModelForm):
+    body = forms.CharField(widget=RedactorEditor(
+        redactor_settings=settings.REDACTOR_SETTINGS))
+
     class Meta:
         model = Post
         fields = ('title', 'body', 'status', 'allow_comments')
