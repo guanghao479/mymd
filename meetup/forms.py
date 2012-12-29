66911ca12ff0d0481e8a9aeea244d230bbe4cd0d
fmaj7 <fmaj7@live.com>
2012-12-29 11:44:17 -0800

    Adding redactor to meetup

--- meetup/forms.py
+++ meetup/forms.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.models import User
 from django import forms
 from meetup.models import Meetup
-
+from redactor.widgets import RedactorEditor
+from django.conf import settings
 
 class MeetupForm(forms.ModelForm):
+    content = forms.CharField(widget=RedactorEditor(
+        redactor_settings=settings.REDACTOR_SETTINGS))
     class Meta:
         model = Meetup
         fields = ('title', 'content', 'date', 'city', 'address', 'poster')
